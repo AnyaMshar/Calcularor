6c29ef06e89b89b1c17d34b4da20ed96e47bcd0c
Hanna <anya.mshar@gmail.com>
2022-06-10 22:19:25 +0100

    Calculator 11.06.2022

--- Calculator-app/Calculator-app/Program.cs
+++ Calculator-app/Calculator-app/Program.cs
@@ -6,18 +6,127 @@ namespace Calculator_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.WriteLine("Enter first number");
+            Menu();
 
-            string num1Str = Console.ReadLine();
-            bool parseRes1 = Int32.TryParse(num1Str, out int num1);
+            var res1 = EnterNumber();
+            
+            if (res1 != -1)
+            {
+                Console.WriteLine("Enter number 1:");
 
-            Console.WriteLine("Give money please");
+                var num1 = EnterNumber();
+
+                if (num1 != -1)
+                {
+                    Console.WriteLine("Enter number 2:");
+
+                    var num2 = EnterNumber();
+
+                    if (num2 != -1)
+                    {
+                        switch (res1)
+                        {
+                            case 1:
+                                Sum(num1, num2);
+                                break;
+                            case 2:
+                                Dif(num1, num2);
+                                break;
+                            case 3:
+                                Mult(num1, num2);
+                                break;
+                            case 4:
+                                Dev(num1, num2);
+                                break;
+                            case 5:
+                                Ost(num1, num2);
+                                break;
+                            default:
+                                Kiss();
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    Mistake();
+                }
+            }
+            else
+            {
+                Mistake();
+            }
+        }
+        
+        public static void Menu()
+        {
+            Console.WriteLine("Press 1 - Sum");
+            Console.WriteLine("Press 2 - Dif");
+            Console.WriteLine("Press 3 - Mult");
+            Console.WriteLine("Press 4 - Dev");
+            Console.WriteLine("Press 5 - Ost");
+            Console.WriteLine("Press 6 - Kiss");
+        }
+        
+        public static void Sum(int a, int b)
+        {
+            int result = a + b;
+            
+            Console.WriteLine($"Sum = {result}");
+        }
+        
+        public static void Dif(int a, int b)
+        {
+            int result = a - b;
+            
+            Console.WriteLine($"Разность чисел = {result}"); ;
+        }
+        
+        public static void Mult(int a, int b)
+        {
+            int result = a * b;
+            
+            Console.WriteLine($"Произведение чисел = {result}");
+        }
+        
+        public static void Dev(int a, int b)
+        {
+            int result = a / b;
+            
+            Console.WriteLine($"Частно чисел = {result}");
+        }
+        
+        public static void Ost(int a, int b)
+        {
+            int result = a % b;
+            
+            Console.WriteLine($"Остаток от деления = {result}");
         }
 
-        void PrintMessage()
+        public static void Kiss()
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Kiss, kiss");
+        }
+
+        public static void Mistake()
+        {
+            Console.WriteLine("Error");
+        }
+
+        public static int EnterNumber()
+        {
+            string enter = Console.ReadLine();
+            bool con = int.TryParse(enter, out int result);
+            if (con)
+            {
+                return result;
+            }
+            else
+            {
+                Mistake();
+                
+                return -1;
+            }
         }
     }
 }

d71a2ab35adfb8bf67383fe1127641b27e27b214
Hanna <anya.mshar@gmail.com>
2022-06-14 22:59:33 +0300

    Add login feature + refactoring

--- Calculator-app/Calculator-app/Program.cs
+++ Calculator-app/Calculator-app/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculator_app
 {
     class Program
     {
+        public static Dictionary<string, string> Users = new Dictionary<string, string>
+        {
+            {"Ilya", "12345"},
+            {"Anya", "987654321"}
+        };
+
         static void Main(string[] args)
         {
-            string name = Hello();
+            Hello();
 
-            HelloName(name);
-            
-            while(true)
+            bool checkUser = TryLogin(out string userName);
+
+            while (checkUser)
             {
                 Menu();
 
@@ -82,14 +89,14 @@ namespace Calculator_app
                 int counter = 0;
 
                 while (true)
-                {  
+                {
                     Next();
 
                     var con1 = EnterNumber();
 
                     if (con1 == 0)
                     {
-                        GoodBye(name);
+                        GoodBye(userName);
                     }
                     else if (con1 == 1)
                     {
@@ -103,17 +110,17 @@ namespace Calculator_app
                         Console.WriteLine($"You have {3 - counter} attempts.");
                         Console.WriteLine();
                         Console.ResetColor();
-                        
+
                         if (counter == 3)
                         {
                             Mistake("You used 3 attempts for entry.");
-                            GoodBye(name);
+                            GoodBye(userName);
                         }
                     }
                 }
             }
         }
-        
+
         public static void Menu()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
@@ -127,7 +134,7 @@ namespace Calculator_app
             Console.ResetColor();
             Console.WriteLine();
         }
-        
+
         public static void Next()
         {
             Console.WriteLine("Do you want to continue?");
@@ -145,17 +152,17 @@ namespace Calculator_app
             Console.ResetColor();
             Console.WriteLine();
         }
-        
+
         public static void Dif(decimal a, decimal b)
         {
             decimal result = a - b;
-            
+
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine($"Difference = {result}");
             Console.ResetColor();
             Console.WriteLine();
         }
-        
+
         public static void Mult(decimal a, decimal b)
         {
             decimal result = a * b;
@@ -165,7 +172,7 @@ namespace Calculator_app
             Console.ResetColor();
             Console.WriteLine();
         }
-        
+
         public static void Div(decimal a, decimal b)
         {
             if (b == 0)
@@ -180,9 +187,9 @@ namespace Calculator_app
                 Console.WriteLine($"Quotient = {result}");
                 Console.ResetColor();
                 Console.WriteLine();
-            } 
+            }
         }
-        
+
         public static void Rem(decimal a, decimal b)
         {
             if (b == 0)
@@ -220,16 +227,16 @@ namespace Calculator_app
             Console.ResetColor();
             Console.WriteLine();
         }
-        
+
         public static int EnterNumber()
         {
             Console.ForegroundColor = ConsoleColor.Green;
             string enter = Console.ReadLine();
             Console.WriteLine();
             Console.ResetColor();
-            
+
             bool con = int.TryParse(enter, out int result);
-            
+
             if (con)
             {
                 return result;
@@ -237,29 +244,55 @@ namespace Calculator_app
             else
             {
                 Mistake("Error. You have to enter a number.");
-                
+
                 return -1;
             }
         }
 
-        public static string Hello()
+        public static void Hello()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Hello. Write your name: ");
+            Console.Write("Hello. ");
             Console.ResetColor();
-
-            string name = Console.ReadLine();
-            Console.WriteLine();
-
-            return name;
         }
 
-        public static void HelloName(string name)
+        public static bool TryLogin(out string name)
         {
-            Console.WriteLine($"Welcome to the Calculator, {name}.");
-            Console.WriteLine();
-        }
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Enter your name: ");
+                Console.ResetColor();
 
+                name = Console.ReadLine();
+                Console.WriteLine();
+
+                var checkName = Users.TryGetValue(name, out string pass);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"{name}, please, write your password: ");
+                Console.ResetColor();
+
+                string password = Console.ReadLine();
+                Console.WriteLine();
+
+                if (checkName && (password == pass))
+                {
+                    Console.WriteLine($"Hello, {name}. Welcome to the Calculator.\n");
+
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You entered the wrong name or password. Try again.\n");
+                    Console.ResetColor();
+                }
+            }
+
+            return true;
+        }
+        
         public static void GoodBye(string name)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
